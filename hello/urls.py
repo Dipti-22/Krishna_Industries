590b51f029d5dafce1acd562bf2efef1d50c8e8f
Dipti Sharma <75442473+Dipti-22@users.noreply.github.com>
2022-07-18 10:50:07 +0530

    changes

--- hello/urls.py
+++ hello/urls.py
@@ -15,6 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+# changes made for static files 
+from django.conf import settings
+from django.conf.urls.static import static
+
+from django.views.static import serve
+from django.conf.urls import url
+
 
 # to change the admin tag line in the admin documentation 
 admin.site.site_header = "Dipti Admin"
@@ -25,4 +32,8 @@ admin.site.index_title = "Welcome to KK Industries"
 urlpatterns = [
     path('admin/', admin.site.urls),     # this will send us to the admin site 
     path('', include('home.urls'))       # this will send the user to the home [ our appilication basic page]
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]    
+
+urlpatterns = urlpatterns+static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
