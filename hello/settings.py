d7f9e1682742614372fbea65a3ab2c678a616c23
Dipti Sharma <75442473+Dipti-22@users.noreply.github.com>
2022-07-18 18:26:07 +0530

    1

--- hello/settings.py
+++ hello/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-@0r4r$04qjeyad%(a+b7_#95g&*$j5*l3ln__9f6@y1$a4-zw#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','krishna-industry.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','krishna-industry.herokuapp.com','krishna-umbrella.herokuapp.com']
 
 
 # Application definition
